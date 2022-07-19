f97a38acc5361a52853c6f72a5d5744eff497faf
Rohit S <rohitsinghal91@yahoo.com>
2022-07-19 07:37:07 +0200

    Add base page class and implement custom commands

---
+++ cypress/pages/BasePage.js
@@ -0,0 +1,44 @@
+class BasePage {
+  constructor() {
+    this.host = Cypress.env('host')
+    this.appUrl = Cypress.env(Cypress.env('host')).url
+  }
+
+  tap = (element) => {
+    return cy.tap(this.getLocator(element))
+  }
+
+  fill = (element, input, clearField) => {
+    return cy.fill(this.getLocator(element), input, clearField)
+  }
+
+  findElement = (element) => {
+    return cy.findElement(this.getLocator(element))
+  }
+
+  checkUrl = (element) => {
+    cy.checkUrl(this.getLocator(element))
+  }
+
+  getLocator = (element) => {
+    return typeof element !== 'object' ? element : element[`${this.app}`]
+  }
+
+  visitUrl = (url) => {
+    cy.visitUrl(url)
+  }
+
+  clearSession = () => {
+    cy.clearSession()
+  }
+
+  clickElementWithText(text, element) {
+    if (element) {
+      cy.get(element).contains(text).click({ force: true })
+    } else {
+      cy.contains(text).click()
+    }
+  }
+}
+
+export default BasePage
